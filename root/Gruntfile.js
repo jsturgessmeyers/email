a0b86a547cceba771690673d637f775b53b5a3ff
Josh Sturgess-Meyers <gtrplyr99@gmail.com>
2016-05-24 16:09:24 -0400

    Updates

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -23,43 +23,17 @@ module.exports = function(grunt) {
                 dest: '_output/index.html'
             }
         },
-        // Run the text converter
-        execute: {
-            simple_target_with_args: {
-                options: {
-                    // execute node with additional arguments
-                    args: ['_output/index.html']
-                },
-                src: ['node_modules/node-text-converter/converter.js']
-            }
-        },
         'special-html': {
             compile: {
                 files: {
                    '_output/index.html': '_output/index.html',
                 }
             }
-        },
-        'phantomjs_screenshot': {
-        main: {
-            options: {
-                delay: 1000
-            },
-            files: [{
-                expand: true,
-                cwd: '_output',
-                src: ['**/*.html'],
-                dest: '_output/screenshots/',
-                ext: '.jpg'
-            }]
         }
-    }
 });
 grunt.loadNpmTasks('grunt-middleman');
 grunt.loadNpmTasks('grunt-prettify');
-grunt.loadNpmTasks('grunt-node-text-converter');
-grunt.loadNpmTasks('grunt-execute');
+
 grunt.loadNpmTasks('grunt-special-html');
-grunt.loadNpmTasks('grunt-phantomjs-screenshot');
-grunt.registerTask('default', ['middleman', 'prettify', 'execute', 'special-html', 'phantomjs_screenshot']);
+grunt.registerTask('default', ['middleman', 'prettify', 'special-html']);
 };
--- template.js
+++ template.js
@@ -1,7 +1,6 @@
 exports.description = "Sets up DRC Email Shell"
 exports.template = function(grunt, init, done) {
     init.process({}, [
-        init.prompt("name","Job Number")
     ], function(err, props) {
         var files = init.filesToCopy(props);
         init.copyAndProcess(files, props);
@@ -9,11 +8,9 @@ exports.template = function(grunt, init, done) {
             name: props.name,
             devDependencies: {
                 "grunt": "^0.4.5",
-                "grunt-execute": "^0.2.2",
                 "grunt-middleman": "^0.1.2",
                 "grunt-prettify": "^0.4.0",
-                "grunt-special-html": "0.0.2",
-                "grunt-phantomjs-screenshot": "^0.1.2"
+                "grunt-special-html": "0.0.2"
             }
         });
         done();
