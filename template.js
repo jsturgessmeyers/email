eed11289969f49a211dce3cd5e7c0c0c83dd2200
Josh Sturgess-Meyers <gtrplyr99@gmail.com>
2016-11-28 16:06:12 -0500

    Fixed grunt version

--- template.js
+++ template.js
@@ -7,7 +7,7 @@ exports.template = function(grunt, init, done) {
         init.writePackageJSON("package.json", {
             name: props.name,
             devDependencies: {
-                "grunt": "^1.0.1",
+                "grunt": "^0.4..1",
                 "grunt-middleman": "^0.2.0",
                 "grunt-prettify": "^0.4.0",
                 "grunt-special-html": "0.0.2"
